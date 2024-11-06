755ec181f91246bd2d3e5960f706631ebcfcb038
JuanGA80 <juangeap@gmail.com>
2024-11-06 17:04:44 -0600

    cambios promos y banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import '../styles/hero.css'
-import Foto1 from '../assets/slider-compu.webp'
-import HeroMov from '../assets/promo-4.webp'
+import Foto1 from '../assets/slider-compu-buen-fin.jpg'
+import HeroMov from '../assets/promo-buen-fin-2.webp'
 
 import '../styles/modal.css'
 
@@ -19,12 +19,12 @@ const Hero = () => {
         className='img-mov'
         loading='eager'
       />
-      <div className='texto-hero'>
+      {/* <div className='texto-hero'>
         <div className='container-texto-hero'>
           <h1>Urban Sonríe</h1>
           <p>Tu mejor opción para volver a sonreír</p>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/components/Promociones.jsx
+++ src/components/Promociones.jsx
@@ -2,7 +2,12 @@ import promotion2 from '../assets/promo-2.webp'
 import promotion3 from '../assets/promo-1.webp'
 import promotion4 from '../assets/promo-3.webp'
 import promotion1 from '../assets/promo-4.webp'
-
+import promoBuenFin1 from '../assets/promo-buen-fin-1.webp'
+import promoBuenFin2 from '../assets/promo-buen-fin-2.webp'
+import promoBuenFin3 from '../assets/promo-buen-fin-3.webp'
+import promoBuenFin4 from '../assets/promo-buen-fin-4.webp'
+import promoBuenFin5 from '../assets/promo-buen-fin-5.webp'
+import promoBuenFin6 from '../assets/promo-buen-fin-6.webp'
 
 const Promociones = () => {
   return (
@@ -13,19 +18,27 @@ const Promociones = () => {
         </div>
         <section className='promociones'>
           <div className='container-promo'>
-            <img src={promotion3} alt='Promoción de blanqueamiento dental' className='promotion-img' />
+            <img src={promoBuenFin1} alt='Promoción de blanqueamiento dental' className='promotion-img' />
             <a href='https://api.whatsapp.com/send?phone=525529484701' target='_blank' rel='noopener noreferrer'>Contáctanos</a>
           </div>
           <div className='container-promo'>
-            <img src={promotion2} alt='Promoción de limpieza dental' className='promotion-img' />
+            <img src={promoBuenFin2} alt='Promoción de limpieza dental' className='promotion-img' />
             <a href='https://api.whatsapp.com/send?phone=525529484701' target='_blank' rel='noopener noreferrer'>Contáctanos</a>
           </div>
           <div className='container-promo'>
-            <img src={promotion4} alt='Promoción extracción de muelas del juicio' className='promotion-img' />
+            <img src={promoBuenFin3} alt='Promoción extracción de muelas del juicio' className='promotion-img' />
             <a href='https://api.whatsapp.com/send?phone=525529484701' target='_blank' rel='noopener noreferrer'>Contáctanos</a>
           </div>
           <div className='container-promo'>
-            <img src={promotion1} alt='Promoción extracción de muelas del juicio' className='promotion-img' />
+            <img src={promoBuenFin4} alt='Promoción extracción de muelas del juicio' className='promotion-img' />
+            <a href='https://api.whatsapp.com/send?phone=525529484701' target='_blank' rel='noopener noreferrer'>Contáctanos</a>
+          </div>
+          <div className='container-promo'>
+            <img src={promoBuenFin5} alt='Promoción extracción de muelas del juicio' className='promotion-img' />
+            <a href='https://api.whatsapp.com/send?phone=525529484701' target='_blank' rel='noopener noreferrer'>Contáctanos</a>
+          </div>
+          <div className='container-promo'>
+            <img src={promoBuenFin6} alt='Promoción extracción de muelas del juicio' className='promotion-img' />
             <a href='https://api.whatsapp.com/send?phone=525529484701' target='_blank' rel='noopener noreferrer'>Contáctanos</a>
           </div>
         </section>
